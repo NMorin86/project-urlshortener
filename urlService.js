19f5f63dce626d88d146d7df3f118dd4a3e21bb9
Glitch (hello-express) <none>
2019-04-06 16:34:03 +0000

    🌛🔮 Checkpoint ./urlService.js:3676277/387 ./package.json:3676277/13

--- urlService.js
+++ urlService.js
@@ -1,9 +1,19 @@
 function makeNew(req, res, next) {
   console.log("In makenew: ", req.body.url);
   
-  URLtoShorten = req.body.url;
-  
+  let URLtoShorten = req.body.url;
+  let returnJSON = shortenURL(URLtoShorten);
+  res.json(returnJSON);
   next();
+}
+
+let dns = require('dns');
+
+function shortenURL(newURL) {
+  let err = dns.lookup(newURL, err => err) === 0)) { // we have a problem
+    return { error: 'Invalid URL. err: ' + err };
+  
+  return {url: newURL };   
 }
 
 function forward(req, res, next) {
