0655eba35ac2dad099e2875f7a6e22e7e3b14f1b
Glitch (hello-express) <none>
2019-04-06 17:38:02 +0000

    🐿🎷 Checkpoint ./urlService.js:3676277/984

--- urlService.js
+++ urlService.js
@@ -16,37 +16,46 @@ function makeNew(req, res, next) {
 }
 
 function shortenURL(newURL, done) {
-    
+console.log("In shorten: ", newURL);    
   dns.lookup(newURL, err => { 
+console.log("In DNS.lookup, err: ", err);
     if(err !== 0) { // we have a problem
       done(0, { error: 'Invalid URL. Error: ' + err });
     } else {    
+console.log("Lookingin DB for duplicate, url: ", newURL);
       URLModel.findOne({ url: newURL }).exec()
         .then(maybeDuplicate => {
           if(maybeDuplicate !== {}) { // we already have a record for this url
             let repeat = { url: maybeDuplicate.url, shortID: maybeDuplicate.shortID };
+console.log("Found duplicate, throwing, repeat: ", repeat);
             return Promise.reject({ duplicate: repeat });
           }
         })
         .then(() => {        
-          return URLModel.countDocuments({}).exec();
+console.log("No dupes, counting documents");
+        return URLModel.countDocuments({}).exec();
         })
         .then(count => {
-          returnJSON = { url: newURL, shortID: count }
+console.log("Count complete, creating new doc, count: ", count);
+        let newShortURL = new URLModel({ url: newURL, shortID: count });
+          return newShortURL.save()
+        })
+        .then(newShortURL => {
+console.log("Doc saved, doc: ", newShortURL);
+        done(null, { url: newShortURL.url, shortID: newShortURL.shortID })
         })
         .catch(err => {
-          if(err.duplicate !== {}) {
-            done(null, err.duplicate)
+console.log("Catching err, err: ", err);
+        if(err.duplicate !== {}) {
+console.log("Returning duplicate doc, doc: ", err.duplicate);
+          done(null, err.duplicate)
           } else {
+console.log("Throwing unspecificed error");
             done(err)
           }
-        });
-        
-          
-    
-  })
-  
-  return {url: newURL };   
+        })
+    }
+  });
 }
 
 function forward(req, res, next) {
