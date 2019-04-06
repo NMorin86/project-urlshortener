976c65a85ea9f22e25e1c778250917ae6ce822d5
Glitch (hello-express) <none>
2019-04-06 17:49:03 +0000

    🎑🚒 Checkpoint ./package.json:3676277/24 ./urlService.js:3676277/370

--- urlService.js
+++ urlService.js
@@ -6,7 +6,7 @@ function makeNew(req, res, next) {
   
   let URLtoShorten = req.body.url;
   shortenURL(URLtoShorten, (err, returnJSON) => {
-    if(err === 0) {
+    if(err === null) {
       res.json(returnJSON);
     } else {
       res.json({error: err})
@@ -16,41 +16,42 @@ function makeNew(req, res, next) {
 }
 
 function shortenURL(newURL, done) {
-console.log("In shorten: ", newURL);    
+                      console.log("In shorten: ", newURL);    
   dns.lookup(newURL, err => { 
-console.log("In DNS.lookup, err: ", err);
-    if(err !== 0) { // we have a problem
-      done(0, { error: 'Invalid URL. Error: ' + err });
+                      console.log("In DNS.lookup, err: ", err);
+    if(err !== null) { // we have a problem
+      done(null, { error: ('Invalid URL. ' + err) });
     } else {    
-console.log("Lookingin DB for duplicate, url: ", newURL);
+                      console.log("Lookingin DB for duplicate, url: ", newURL);
       URLModel.findOne({ url: newURL }).exec()
         .then(maybeDuplicate => {
-          if(maybeDuplicate !== {}) { // we already have a record for this url
+                      console.log("testing duplicate, dupe: ", maybeDuplicate);
+        if(maybeDuplicate !== null) { // we already have a record for this url
             let repeat = { url: maybeDuplicate.url, shortID: maybeDuplicate.shortID };
-console.log("Found duplicate, throwing, repeat: ", repeat);
+                      console.log("Found duplicate, throwing, repeat: ", repeat);
             return Promise.reject({ duplicate: repeat });
           }
         })
         .then(() => {        
-console.log("No dupes, counting documents");
+                      console.log("No dupes, counting documents");
         return URLModel.countDocuments({}).exec();
         })
         .then(count => {
-console.log("Count complete, creating new doc, count: ", count);
+                      console.log("Count complete, creating new doc, count: ", count);
         let newShortURL = new URLModel({ url: newURL, shortID: count });
           return newShortURL.save()
         })
         .then(newShortURL => {
-console.log("Doc saved, doc: ", newShortURL);
+                      console.log("Doc saved, doc: ", newShortURL);
         done(null, { url: newShortURL.url, shortID: newShortURL.shortID })
         })
         .catch(err => {
-console.log("Catching err, err: ", err);
+                      console.log("Catching err, err: ", err);
         if(err.duplicate !== {}) {
-console.log("Returning duplicate doc, doc: ", err.duplicate);
+                      console.log("Returning duplicate doc, doc: ", err.duplicate);
           done(null, err.duplicate)
           } else {
-console.log("Throwing unspecificed error");
+                      console.log("Throwing unspecificed error");
             done(err)
           }
         })
